21cece75aef83c3cfa4b509a5c8afba1fb5dca4e
oli <oli@meimberg.io>
2025-01-19 00:17:28 +0100

    urls

--- config/server.ts
+++ config/server.ts
@@ -1,6 +1,12 @@
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: env('SERVER_URL'),
+  public_url: env('PUBLIC_URL'),
+
+  admin: {
+    url: env('ADMIN_URL'),
+  },
   app: {
     keys: env.array('APP_KEYS'),
   },
